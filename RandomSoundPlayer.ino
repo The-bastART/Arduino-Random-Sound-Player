b5c17081a8c690ee10eaa4a64c03866760e86cff
The-bastART <tilmann.moeller@gmx.de>
2019-10-17 22:53:50 +0200

    Add files via upload

---
+++ RandomSoundPlayer.ino
@@ -0,0 +1,43 @@
+#include "SD.h"
+#include "TMRpcm.h"
+#define SD_ChipSelectPin 10
+
+TMRpcm tmrpcm;
+
+int buttonState = 0;
+int lastButtonState = 0; 
+
+void setup(){ 
+tmrpcm.speakerPin=9;
+tmrpcm.quality(1);
+tmrpcm.setVolume(5);
+randomSeed(analogRead(0));
+if(!SD.begin(SD_ChipSelectPin)){
+  Serial.println("SD fail");
+  return;}
+Serial.begin(9600);
+}
+
+void LetTheMusicPlay(){
+  char filename[7];
+  sprintf(filename, "%02d.wav", random(51));
+  Serial.println(filename);
+  Serial.println("---");
+  tmrpcm.play(filename);
+  delay(1000);
+  
+}
+
+void loop(){
+  buttonState = digitalRead(4);
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+      Serial.println("on");
+      LetTheMusicPlay();
+    } else {
+      Serial.println("off");
+    }
+    delay(50);
+  }
+  lastButtonState = buttonState;
+}
